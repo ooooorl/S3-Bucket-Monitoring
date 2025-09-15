803b2e58aeff41172d7285b201018723d68823d4
orlyplaza <plaza.orly.omeles@gmail.com>
2025-09-15 21:13:56 +0800

    feat(s3/main.tf): remove PutObject bucket permission

--- modules/s3-bucket/main.tf
+++ modules/s3-bucket/main.tf
@@ -39,8 +39,9 @@ data "aws_iam_policy_document" "bucket_policy" {
   # GetObject and PutObject apply to objects under the bucket
   statement {
     effect = "Allow"
-    actions   = ["s3:GetObject", "s3:PutObject"]
+    actions   = ["s3:GetObject"]
     resources = ["${aws_s3_bucket.this.arn}/*"]
+    # actions   = ["s3:GetObject", "s3:PutObject"]
 
     principals {
       type        = "AWS"
