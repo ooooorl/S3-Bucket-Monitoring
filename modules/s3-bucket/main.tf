862a74294399371f1e4fdba5c65e9e0cbb2da64a
orlyplaza <plaza.orly.omeles@gmail.com>
2025-09-19 22:45:37 +0800

    feat(s3-bucket/main.tf): add CloudTrail integration for S3 bucket policy change detection
    - Provision CloudTrail trail to capture S3 control plane API calls
    - Updated S3 bucket policy to allow CloudTrail service:
      - Read bucket ACLs (AWSCloudTrailAclCheck)
      - Write CloudTrail logs with bucket-owner-full-control (AWSCloudTrailWrite)
    - Added EventBridge rule to filter S3 bucket policy/ACL changes
    - Connected EventBridge to Lambda for logging events into CloudWatch

--- modules/s3-bucket/main.tf
+++ modules/s3-bucket/main.tf
@@ -18,6 +18,11 @@ resource "aws_s3_bucket_versioning" "this" {
   }
 }
 
+# Add caller identity for CloudTrail bucket policy
+# Pulls your AWS account ID automatically
+# This is inserted into the bucket policy for the log path (AWSLogs/<account-id>).
+data "aws_caller_identity" "current" {}
+
 resource "aws_s3_bucket_policy" "this" {
   bucket = aws_s3_bucket.this.id
   policy = data.aws_iam_policy_document.bucket_policy.json
@@ -39,12 +44,47 @@ data "aws_iam_policy_document" "bucket_policy" {
   # GetObject and PutObject apply to objects under the bucket
   statement {
     effect = "Allow"
-    actions   = ["s3:GetObject", "s3:PutObject"]
+    actions   = ["s3:GetObject", "s3:PutObject", "s3:DeleteObject"]
     resources = ["${aws_s3_bucket.this.arn}/*"]
 
     principals {
       type        = "AWS"
       identifiers = var.allowed_roles
+    }
+  }
+
+  # Allow CloudTrail to write logs to this bucket
+  # CloudTrail always checks bucket ACLs before writing.
+  statement {
+    sid     = "AWSCloudTrailAclCheck"
+    effect  = "Allow"
+    actions = ["s3:GetBucketAcl"]
+    resources = [aws_s3_bucket.this.arn]
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudtrail.amazonaws.com"]
+    }
+  }
+
+  statement {
+    sid     = "AWSCloudTrailWrite"
+    effect  = "Allow"
+    actions = ["s3:PutObject"]
+    resources = [
+      "${aws_s3_bucket.this.arn}/AWSLogs/${data.aws_caller_identity.current.account_id}/*"
+    ]
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudtrail.amazonaws.com"]
+    }
+
+    # The condition ensures logs are written with ACL = bucket-owner-full-control (so you never lose ownership).
+    condition {
+      test     = "StringEquals"
+      variable = "s3:x-amz-acl"
+      values   = ["bucket-owner-full-control"]
     }
   }
 }
