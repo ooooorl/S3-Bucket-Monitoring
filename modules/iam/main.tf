71d7c768b970b0a2fff69c680ee85dff16fb565a
orlyplaza <plaza.orly.omeles@gmail.com>
2025-09-20 21:35:46 +0800

    docs: add comment on lambda policy

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -41,6 +41,7 @@ resource "aws_iam_role" "lambda_exec" {
   })
 }
 
+# Attaches the AWSLambdaBasicExecutionRole managed policy to the Lambda execution role
 resource "aws_iam_role_policy_attachment" "lambda_basic" {
   role       = aws_iam_role.lambda_exec.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
